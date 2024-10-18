c3450c422ac4b7f33051a1ee7751d33b5a376b04
yj <153605289+yjcstudy1@users.noreply.github.com>
2024-10-18 13:51:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]; // 交換位置
+    [array[i], array[j]] = [arra   
+y[j], array[i]]; // 交換位置
   }
   return array;
 }
 
-urls = shuffle(urls); // 將 urls 陣列進行洗牌
-window.location.href = urls.length > 0 ? urls[0] : 'https://yjcstudy1.github.io/Testsurvey/';
+urls = shuffle(urls);
+
+if (urls.length > 0) {
+  window.location.href = urls[0];
+} else {
+  console.error('URL陣列为空，无法进行导向');
+}
