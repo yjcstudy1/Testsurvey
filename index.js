3274ce0f3e11df06e368991e2a45f8c342ddaf44
yj <153605289+yjcstudy1@users.noreply.github.com>
2024-10-18 14:14:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-window.location.href = urls.length > 0 ? urls[Math.floor(Math.random() * urls.length)] : 'https://yjcstudy1.github.io/survey2/';
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // 交換位置
+  }
+  return array;
+}
+
+const shuffledUrls = shuffle(urls); // 將 urls 陣列進行洗牌
+window.location.href = shuffledUrls.length > 0 ? shuffledUrls[0] : 'https://yjcstudy1.github.io/survey2/';
