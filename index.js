9e1884a50da0fe905381053c31cd272284602299
yj <153605289+yjcstudy1@users.noreply.github.com>
2024-10-18 13:57:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [arra   
-y[j], array[i]]; // 交換位置
-  }
-  return array;
-}
-
-urls = shuffle(urls);
-
-if (urls.length > 0) {
-  window.location.href = urls[0];
-} else {
-  console.error('URL陣列为空，无法进行导向');
-}
+window.location.href = urls.length > 0 ? urls[Math.floor(Math.random() * urls.length)] : 'https://yjcstudy1.github.io/survey2/';
